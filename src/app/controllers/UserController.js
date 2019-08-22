1ef26161ed795a0e9e9d5d436e1945888b85c6e2
José Rafael <zerafael1@gmail.com>
2019-08-22 18:42:58 -0400

    Added database connection. Added User model Added User controller Added create user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.send('Hello World'));
+// Create user
+routes.post('/users', UserController.store);
 
 export default routes;
