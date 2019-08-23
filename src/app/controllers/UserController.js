81f20c440c9d565b58a2953e03eb9723c1663854
José Rafael <zerafael1@gmail.com>
2019-08-22 20:43:27 -0400

    Added validation on user creation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email } = req.body;
 
     const userExists = await User.findOne({ where: { email } });
