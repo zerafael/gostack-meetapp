e49894462d4d20cfc3aae829fc42a9f2b7eda6ec
José Rafael <zerafael1@gmail.com>
2019-08-26 12:32:55 -0400

    Add file management

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middleware/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Create user
 routes.post('/users', UserController.store);
@@ -18,5 +22,7 @@ routes.use(authMiddleware);
 
 // Update user
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
