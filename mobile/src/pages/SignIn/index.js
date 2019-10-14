7052095cfcbff539b1151b5fa352e4085f4880a0
José Rafael <zerafael1@gmail.com>
2019-10-14 01:15:15 -0400

    Add routes to SignIn and SignUp Add background gradient Eslint, prettier and root import configured

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
+
+function SignIn() {
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  );
+}
+
+export default SignIn;
---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
+
+function SignUp() {
+  return (
+    <Background>
+      <Text>SignUp</Text>;
+    </Background>
+  );
+}
+
+export default SignUp;
