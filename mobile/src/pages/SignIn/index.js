3fd8e34242795f4fb098e2f6d7f8546ff3d8f53f
José Rafael <zerafael1@gmail.com>
2019-10-14 23:45:15 -0400

    SignIn and SignUp screens added

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-function SignIn() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  LinkText,
+} from './styles';
+
+function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input style={{ marginTop: 30 }} placeholder="Digite seu nome" />
-      <Button>Teste</Button>
+        <Form>
+          <FormInput
+            placeholder="Digite seu e-mail"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <FormInput
+            placeholder="Sua senha secreta"
+            secureTextEntry
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={() => {}}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SubmitButton>Entrar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar conta grátis</LinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  LinkText,
+} from './styles';
+
+function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Background>
-      <Text>SignUp</Text>;
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            placeholder="Nome completo"
+            autoCorrect={false}
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+          <FormInput
+            placeholder="Digite seu e-mail"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <FormInput
+            placeholder="Sua senha secreta"
+            secureTextEntry
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={() => {}}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <SubmitButton>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <LinkText>Já tenho conta</LinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
