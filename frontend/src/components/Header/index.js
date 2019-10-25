138842bf7780f8e77e97bcc9a2ddf5640b0c7f72
José Rafael <zerafael1@gmail.com>
2019-10-25 17:15:33 -0400

    Use user name on frontend header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo.png';
@@ -10,6 +10,8 @@ import { signOut } from '~/store/modules/auth/actions';
 
 function Header() {
   const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
 
   function handleSignOut() {
     dispatch(signOut());
@@ -24,7 +26,7 @@ function Header() {
 
         <Profile>
           <div>
-            <strong>Fulano de tal</strong>
+            <strong>{profile.name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </div>
           <button type="button" onClick={handleSignOut}>
