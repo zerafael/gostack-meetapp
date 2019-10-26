b68fc1ede81b991bae155167ed83df585cced332
José Rafael <zerafael1@gmail.com>
2019-10-26 14:05:40 -0400

    Profile validation

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { MdControlPoint } from 'react-icons/md';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email valido')
+    .required('O email é obrigatório'),
+  oldPassword: Yup.string()
+    .min(6)
+    .transform(value => (!value ? null : value))
+    .nullable(),
+  password: Yup.string()
+    .transform(value => (!value ? null : value))
+    .nullable()
+    .min(6)
+    .when('oldPassword', (oldPassword, field) =>
+      oldPassword ? field.required() : field
+    ),
+  confirmPassword: Yup.string().when('password', (password, field) =>
+    password ? field.required().oneOf([Yup.ref('password')]) : field
+  ),
+});
 
 function Profile() {
   const dispatch = useDispatch();
@@ -18,7 +40,7 @@ function Profile() {
 
   return (
     <Container>
-      <Form initialData={profile} onSubmit={handleSubmit}>
+      <Form initialData={profile} schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de email" />
 
