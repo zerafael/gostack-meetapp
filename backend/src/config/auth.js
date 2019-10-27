d37df087cbbc16f0e4593ced12ed769f3fb3f238
José Rafael <zerafael1@gmail.com>
2019-10-27 17:05:28 -0400

    Add environment variables

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '155a3bdc98a5e4720a9d8e6a9e532415',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,12 +1,12 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c62a37d085cd17',
-    pass: '177a499e7c6ab2',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
-    from: 'Equipe MeetUp <noreplay@meetup.com',
+    from: 'Equipe MeetApp <noreplay@meetapp.com',
   },
 };
